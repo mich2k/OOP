98d46f08c16213c1210354c8d3d6ed05cd3deafb
mich2k <miky3848@gmail.com>
2021-03-09 13:17:58 +0100

    formal package java way

---
+++ src/org/gmichele/Costrutti.java
@@ -0,0 +1,8 @@
+package org.gmichele;
+
+public static class Costrutti {
+    public static void main(String [] args){
+        for(int i = 10; i > 0; i--)
+            System.out.println("hey " + i);
+    }
+}
---
+++ src/org/gmichele/Hello.java
@@ -0,0 +1,8 @@
+package org.gmichele;
+
+public static class Hello {    // Stesso nome del file
+    public static void main(String[] args) {     // Standard di scrittura per il main della classe
+        final double PI = 3.1415; // final = const, le final DEVONO essere tutte UPPERCASE
+        System.out.println("Rickrolled");
+    }
+}
