8286b1386ad1d9b819097cd694d075b22cff204e
mich2k <miky3848@gmail.com>
2021-03-09 10:14:43 +0100

    Fino a E03

---
+++ firstApp/src/JavaConventions.java
@@ -0,0 +1,10 @@
+public class JavaConventions {  // nome classe con lettera maiuscola
+        final double PI = 3.14; // prima le costanti, uppercase;
+        private int nomeAttributo;  // non la trova il builder da eseguire siccome non ha classe standard main
+        public void nomeMetodo(){   // si mettono prima le eventuali private e poi le pubbliche
+            int var = 3;
+            if(var == 0){
+                // something;
+                 }
+    }
+}
---
+++ firstApp/src/costrutti.java
@@ -0,0 +1,6 @@
+public class costrutti {
+    public static void main(String [] args){
+        for(int i = 10; i > 0; i--)
+            System.out.println("hey " + i);
+    }
+}
