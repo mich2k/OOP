183a472f1b90c420021ee60ec312197dd6ae0aef
mich2k <miky3848@gmail.com>
2021-07-20 23:17:17 +0200

    java

---
+++ Python/main.py
@@ -0,0 +1,15 @@
+
+
+def main():
+    my_dict = [
+        {1: primo},
+        {2:secondo}
+    ]
+
+for k,v in my_dict:
+    print(f"la chiave e' {k} e il valore {v}")
+
+    return
+
+if __name__ == '__main__'():
+    main()
